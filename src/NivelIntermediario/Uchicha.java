170603328ad9e2ef4d5c922cfe95f9e32f31abd4
Felipe <felipemiguelfutebol@hotmail.com>
2026-05-19 19:43:10 -0300

    Removendo pastas do repositorio

--- src/NivelIntermediario/Uchicha.java
+++
@@ -1,7 +0,0 @@
-package NivelIntermediario;
-
-public class Uchicha extends ninja {
-    public void sharinganAtivado (){
-        System.out.println("O meu nome é " + nome + " e acabei de ativar o sharingan!");
-    }
-}
--- src/NivelIntermediario/Uzumaki.java
+++
@@ -1,7 +0,0 @@
-package NivelIntermediario;
-
-public class Uzumaki extends ninja {
-    public void ChakaraAlto() {
-        System.out.println("O ninja " + nome + " tem o chakara mais alto que os outros ninjas!");
-    }
-}
